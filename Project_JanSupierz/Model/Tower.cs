7a84def00df46293f1e6e05803283472d68df2aa
Janek2002 <jan.supierz02@gmail.com>
2023-03-21 20:52:20 +0100

    Basic tower model

---
+++ Project_JanSupierz/Model/Tower.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_JanSupierz.Model
+{
+    public class Cost
+    {
+        public int Easy { get; set; }
+        public int Medium { get; set; }
+        public int Hard { get; set; }
+        public int Impoppable { get; set; }
+    }
+
+    public class Stats
+    {
+        public string Damage { get; set; }
+        public string Pierce { get; set; }
+        public string AttackSpeed { get; set; }
+        public string Range { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class Tower
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+
+        public int Footprint { get; set; }
+        public string DefaultHotkey { get; set; }
+
+        public Cost Cost { get; set; }
+        public Stats Stats { get; set; }
+    }
+}

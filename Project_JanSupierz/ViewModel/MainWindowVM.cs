4985d074a6f9184f9475966fd9e8275b128bef1a
Janek2002 <jan.supierz02@gmail.com>
2023-03-21 21:31:30 +0100

    Displaying Tower page
    - added main view model
    - added tower page

---
+++ Project_JanSupierz/ViewModel/MainWindowVM.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Project_JanSupierz.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Project_JanSupierz.ViewModel
+{
+    public class MainWindowVM: ObservableObject
+    {
+        public Page CurrentPage {  get; set; }
+        private TowerPage TowerPage { get; } = new TowerPage();
+
+        public MainWindowVM()
+        {
+            CurrentPage = TowerPage;
+        }
+    }
+}
